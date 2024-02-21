fc05b6accd079b59fc56c2b65e93f58e3746b7ea
zoozzzz <zoozzzz@users.noreply.github.com>
2024-02-21 03:49:51 +0000

    Automated deployment: Wed Feb 21 03:49:51 UTC 2024 212f71872ee28ae35fa378ea544282d33dd04940

---
+++ assets/AboutView.29c6b4dd.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as _}from"./index.66f54bef.js";const s={},c={class:"about"},n=_("h1",null,"about",-1),a=[n];function r(i,u){return t(),o("div",c,a)}const l=e(s,[["render",r]]);export{l as default};
